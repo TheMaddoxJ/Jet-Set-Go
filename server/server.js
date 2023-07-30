7b8484f58e62315928169e0f27bd758538a5b600
floretdemola <floretdemola@gmail.com>
2023-07-30 19:08:26 -0400

    added models schema updated server js and installed express express-graphql graphql mongoose jsonwebtoken dotenv

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { graphqlHTTP } = require('express-graphql');
+const  mongoose = require("mongoose");
+const schema = require('./schema');
+const resolvers = require('./schema/resolvers')
+
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
@@ -11,6 +16,14 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.losg('MongoDB connection error', err));
+
 // instance of Apollo server. pass in schema data
 const server = new ApolloServer({
   typeDefs,
@@ -21,3 +34,7 @@ const server = new ApolloServer({
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`🌎 Server running on port ${PORT}!`);
+})
