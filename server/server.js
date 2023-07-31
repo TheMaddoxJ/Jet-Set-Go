ff6130ee1e61ed23d6fc95bcf02c6dc2c2936771
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-07-31 19:58:16 -0400

    server.js changes added

--- server/server.js
+++ server/server.js
@@ -21,3 +21,30 @@ const server = new ApolloServer({
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// serve client/build as static assets while in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"));
+});
+
+// new instance of an Apollo server with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call async function to start the server
+startApolloServer(typeDefs, resolvers);
